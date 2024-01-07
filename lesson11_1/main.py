70b98bb6ca2399860b588eb1964e5fe627641d0e
changweiyang <150911097+changweiyang@users.noreply.github.com>
2024-01-07 13:44:31 +0000

    modify

--- lesson11_1/main.py
+++ lesson11_1/main.py
@@ -6,7 +6,7 @@ if __name__ == '__main__':
     o_nums:int = pyip.inputInt("請輸入科目數(1~7):",min=1,max=7)
     students:list[list] = getStudents(student_nums=s_nums,scores_nums=o_nums)
     fileName = pyip.inputFilename("請輸入檔案名稱(不用輸入副檔名稱):")
-    if saveToCSV(fileName=fileName,data=students,subjects_nums=o_nums)
+    if saveToCSV(fileName=fileName,data=students,subjects_nums=o_nums):
         print('存檔成功')
     else:
         print('存檔失敗')
